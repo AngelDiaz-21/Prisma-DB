8d9a576ca4148fd96ef0b51c9c2b757d167d4c79
Angel <angelSistemas21@gmail.com>
2022-09-03 23:10:52 -0500

    feat: API CRUD para el modelo mission_commander

--- server.js
+++ server.js
@@ -58,3 +58,49 @@ app.delete('/explorers/:id', async (req, res) => {
     await prisma.explorer.delete({ where: { id: id } });
     return res.json({ message: "Eliminado correctamente" });
 });
+
+// table/model mission_commander
+app.get('/missionCommander', async (req, res) => {
+    const allMissionCommanders = await prisma.mission_commanders.findMany({});
+    res.json(allMissionCommanders);
+});
+
+app.get('/missionCommander/:id', async (req, res) => {
+    const id = req.params.id;
+    const mission_commander = await prisma.mission_commanders.findUnique({ where: { id: parseInt(id) } });
+    res.json(mission_commander);
+});
+
+app.post('/missionCommander', async (req, res) => {
+    const mission_commander = {
+        name: req.body.name,
+        missionCommander: req.body.missionCommander,
+        enrollments: req.body.enrollments
+    };
+    const message = 'Mission commander creado.';
+    await prisma.mission_commanders.create({ data: mission_commander });
+    return res.json({ message });
+});
+
+app.put('/missionCommander/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    await prisma.mission_commanders.update({
+        where: {
+            id: id
+        },
+        data: {
+            name: req.body.name,
+            missionCommander: req.body.missionCommander,
+            enrollments: req.body.enrollments
+        }
+    })
+
+    return res.json({ message: "Actualizado correctamente" });
+});
+
+app.delete('/missionCommander/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.mission_commanders.delete({ where: { id: id } });
+    return res.json({ message: "Eliminado correctamente" });
+});
